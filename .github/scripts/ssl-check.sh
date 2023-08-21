2da9338cbcc4889e42c4bee29cca82abed02cf4a
pavan rayabagi <68803406+pavan731@users.noreply.github.com>
2023-08-21 21:25:33 +0530

    Update and rename ssl_check.sh to ssl-check.sh

---
+++ .github/scripts/ssl-check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SLACK_WEBHOOK_URL=$1
+DOMAIN_LIST=$2
+
+function sendSlackNotification {
+  local domain=$1
+  local daysLeft=$2
+
+  local message="SSL Expiry Alert\n"
+  message+="   * Domain: $domain\n"
+  message+="   * Warning: The SSL certificate for $domain will expire in $daysLeft days."
+
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$SLACK_WEBHOOK_URL"
+}
+
+while IFS= read -r domain; do
+  expiry_date=$(openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -enddate | sed 's/notAfter=//')
+  expiry_timestamp=$(date -d "$expiry_date" +%s)
+  current_timestamp=$(date +%s)
+  days_left=$(( ($expiry_timestamp - $current_timestamp) / 86400 ))
+
+  if [ "$days_left" -lt 30 ]; then
+    sendSlackNotification "$domain" "$days_left"
+  fi
+done <<< "$DOMAIN_LIST"
