596408e0eeb7402d5f7a95f71bb4faef9b7d7212
pavan rayabagi <68803406+pavan731@users.noreply.github.com>
2023-08-21 22:53:26 +0530

    Update ssl-check.sh

--- .github/scripts/ssl-check.sh
+++ .github/scripts/ssl-check.sh
@@ -6,6 +6,7 @@ SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL
 function sendSlackNotification {
   local message="$1"
 
+  echo "Sending message to Slack: $message"
   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$SLACK_WEBHOOK_URL"
 }
 
@@ -13,11 +14,11 @@ function sendSlackNotification {
 
 for DOMAIN in $DOMAINLIST
 do
-  EXPIRY=$( echo | openssl s_client -servername $DOMAIN -connect $DOMAIN:443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | sed 's/notAfter=//')
-  EXPIRYSIMPLE=$( date -d "$EXPIRY" +%F )
-  EXPIRYSEC=$(date -d "$EXPIRY" +%s)
-  TODAYSEC=$(date +%s)
-  EXPIRYCALC=$(echo "($EXPIRYSEC-$TODAYSEC)/86400" | bc )
+  EXPIRY_DATE=$( echo | openssl s_client -servername $DOMAIN -connect $DOMAIN:443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | sed 's/notAfter=//' )
+  EXPIRYSIMPLE=$( date -d "$EXPIRY_DATE" +%F )
+  EXPIRYSEC=$( date -d "$EXPIRY_DATE" +%s )
+  TODAYSEC=$( date +%s )
+  EXPIRYCALC=$(( ($EXPIRYSEC - $TODAYSEC) / 86400 ))
   # Output
   if [ $EXPIRYCALC -lt $EXPIRYALERTDAYS ] ;
   then
@@ -26,6 +27,31 @@ do
   fi
   echo "$EXPIRYSIMPLE - $DOMAIN expires (in $EXPIRYCALC days)"
 done
+
+
+# function sendSlackNotification {
+#   local message="$1"
+
+#   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$SLACK_WEBHOOK_URL"
+# }
+
+# # ... (rest of your script)
+
+# for DOMAIN in $DOMAINLIST
+# do
+#   EXPIRY=$( echo | openssl s_client -servername $DOMAIN -connect $DOMAIN:443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | sed 's/notAfter=//')
+#   EXPIRYSIMPLE=$( date -d "$EXPIRY" +%F )
+#   EXPIRYSEC=$(date -d "$EXPIRY" +%s)
+#   TODAYSEC=$(date +%s)
+#   EXPIRYCALC=$(echo "($EXPIRYSEC-$TODAYSEC)/86400" | bc )
+#   # Output
+#   if [ $EXPIRYCALC -lt $EXPIRYALERTDAYS ] ;
+#   then
+#     alert_message="######ALERT####### $DOMAIN Cert needs to be renewed."
+#     sendSlackNotification "$alert_message"
+#   fi
+#   echo "$EXPIRYSIMPLE - $DOMAIN expires (in $EXPIRYCALC days)"
+# done
 
 #   if [ "$days_left" -lt 30 ]; then
 #     sendSlackNotification "$domain" "$days_left"
